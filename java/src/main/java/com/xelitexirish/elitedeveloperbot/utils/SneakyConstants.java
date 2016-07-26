531ac9d7fd65f4a81e85080fe27775cbe10747c3
XeliteXirish <shaunoneill7@me.com>
2016-07-26 23:23:14 +0100

    Not needed, token is passed during startup

--- java/src/main/java/com/xelitexirish/elitedeveloperbot/utils/SneakyConstants.java
+++
@@ -1,6 +0,0 @@
-package com.xelitexirish.elitedeveloperbot.utils;
-
-public class SneakyConstants {
-
-    public static final String DISCORD_TOKEN = "MjA3MTg5Nzc1Mjk4NDYxNjk2.CnfgAw.KkRzr2jA_ssxZbzUpt-D_ucF8kk";
-}
