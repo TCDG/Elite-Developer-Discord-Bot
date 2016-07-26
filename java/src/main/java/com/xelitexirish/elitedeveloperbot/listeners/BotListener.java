982da18fe117d07b6a89ced340b6aa6ddfbdf964
XeliteXirish <shaunoneill7@me.com>
2016-07-26 20:20:17 +0100

    enable welcome and leave messages

--- java/src/main/java/com/xelitexirish/elitedeveloperbot/listeners/BotListener.java
+++ java/src/main/java/com/xelitexirish/elitedeveloperbot/listeners/BotListener.java
@@ -3,7 +3,11 @@ package com.xelitexirish.elitedeveloperbot.listeners;
 import com.xelitexirish.elitedeveloperbot.Main;
 import com.xelitexirish.elitedeveloperbot.utils.Constants;
 import com.xelitexirish.elitedeveloperbot.utils.Logger;
+import com.xelitexirish.elitedeveloperbot.utils.MessageUtils;
 import net.dv8tion.jda.events.ReadyEvent;
+import net.dv8tion.jda.events.guild.member.GuildMemberBanEvent;
+import net.dv8tion.jda.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.hooks.ListenerAdapter;
 
@@ -14,6 +18,18 @@ public class BotListener extends ListenerAdapter {
         if (event.getMessage().getContent().startsWith(Constants.COMMAND_PREFIX) && event.getMessage().getAuthor().getId() != event.getJDA().getSelfInfo().getId()) {
             Main.handleCommand(Main.parser.parse(event.getMessage().getContent().toLowerCase(), event));
         }
+    }
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        String welcomeMessage = "Welcome " + event.getUser().getUsername() + " make sure you read the rules!";
+        event.getGuild().getPublicChannel().sendMessage(welcomeMessage);
+    }
+
+    @Override
+    public void onGuildMemberBan(GuildMemberBanEvent event) {
+        String banMessage = "The ban hammer has spoken!";
+        event.getGuild().getPublicChannel().sendMessage(MessageUtils.wrapStringInCodeBlock(banMessage));
     }
 
     @Override
