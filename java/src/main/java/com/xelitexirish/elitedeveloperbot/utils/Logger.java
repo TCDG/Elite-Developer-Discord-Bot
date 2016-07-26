ae9465547f089ee1f0d54af8ca100cdd5c48aa72
XeliteXirish <shaunoneill7@me.com>
2016-07-26 22:41:34 +0100

    Logger file

--- java/src/main/java/com/xelitexirish/elitedeveloperbot/utils/Logger.java
+++ java/src/main/java/com/xelitexirish/elitedeveloperbot/utils/Logger.java
@@ -1,9 +1,28 @@
 package com.xelitexirish.elitedeveloperbot.utils;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 public class Logger {
+
+    public static PrintWriter writer;
+
+    public void initLogger(){
+        try {
+            writer = new PrintWriter("logger.txt", "UTF-8");
+            writer.println("#Start of log file");
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void log(String heading, String message){
         System.out.println("[" + heading.toUpperCase() + "] " + message);
+        writer.println("[" + heading.toUpperCase() + "] " + message);
     }
 
     public static void info(String message){
@@ -11,6 +30,6 @@ public class Logger {
     }
 
     public static void command(String title, String username){
-        log("COMAND: " + title, "Issued by player: " + username);
+        log("COMMAND: " + title, "Issued by player: " + username);
     }
 }
