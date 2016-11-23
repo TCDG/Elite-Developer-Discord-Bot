f662fa5683662ee513195e1b42dd9c356abb43a5
Lyxnx <a.julnes@gmail.com>
2016-11-23 20:28:36 +0000

    Edit abstract bot class

--- api/src/main/java/com/collectivedev/edb/api/IBot.java
+++ api/src/main/java/com/collectivedev/edb/api/IBot.java
@@ -1,8 +1,19 @@
 package com.collectivedev.edb.api;
 
 import com.collectivedev.edb.api.persist.AbstractDataStore;
+import com.collectivedev.edb.api.persist.JsonFile;
+import com.google.gson.JsonObject;
+import net.dv8tion.jda.JDA;
 
 public abstract class IBot {
     
+    public void load() throws Exception { }
+    
+    public void unload() { }
+    
     public abstract AbstractDataStore getDataStore();
+    
+    public abstract JsonFile<JsonObject> getJsonConfig();
+    
+    public abstract JDA getJDA();
 }
