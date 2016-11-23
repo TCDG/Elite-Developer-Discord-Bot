3bbcec54cd2eec87b89a929faf855b8ba2462a39
Lyxnx <a.julnes@gmail.com>
2016-11-23 20:31:18 +0000

    Create separate main class for loading the bot

--- bot/src/main/java/com/collectivedev/edb/bot/Main.java
+++ bot/src/main/java/com/collectivedev/edb/bot/Main.java
@@ -2,5 +2,8 @@ package com.collectivedev.edb.bot;
 
 public class Main {
     
-    
+    public static void main(String[] args) throws Exception {
+        EDB edb = new EDB();
+        edb.load();
+    }
 }
